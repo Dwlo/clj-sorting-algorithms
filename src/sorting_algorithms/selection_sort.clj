82d9764c390c881d34f1827cdde30caff293d64a
Dw <lo.dawid@gmail.com>
2014-12-20 11:37:56 +0000

    better names

--- src/sorting_algorithms/selection_sort.clj
+++ src/sorting_algorithms/selection_sort.clj
@@ -3,11 +3,11 @@
   (:require [sorting-algorithms.utils :as  ut]))
 
 (defn process
-  [ordered-ns ns]
-  (if (empty? ns)
-    ordered-ns
-    (let [min-val (apply min ns)]
-      (process (cons min-val ordered-ns) (ut/remove-item min-val ns)))))
+  [ordered-items messy-items]
+  (if (empty? messy-items)
+    ordered-items
+    (let [min-val (apply min messy-items)]
+      (process (cons min-val ordered-items) (ut/remove-item min-val messy-items)))))
 
 (defn rsort
   "Sorts in reverse order using the selection sort algorithm.
@@ -17,5 +17,5 @@
   [4* 3* 1  2 ]
   [4* 3* 2* 1 ]
   [4* 3* 2* 1*]"
-  [numbers]
-  (process [] numbers))
+  [items]
+  (process [] items))
