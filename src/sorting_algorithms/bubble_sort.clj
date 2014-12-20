82710d20ddf8bfae408825b82a13ee6b5f262090
Dw <lo.dawid@gmail.com>
2014-12-20 10:52:43 +0000

    feat: bubble sort algorithm corrected

--- src/sorting_algorithms/bubble_sort.clj
+++ src/sorting_algorithms/bubble_sort.clj
@@ -1,15 +1,35 @@
 (ns sorting-algorithms.bubble-sort
-  "Sorting using the bubble sort algorithm."
-  (:require [sorting-algorithms.utils :as  ut]))
+  "Sorting using the bubble sort algorithm.")
 
-(defn process
-  [ordered-ns ns]
-  (if (empty? ns)
-    ordered-ns
-    (let [min-val (apply min ns)]
-      (process (cons min-val ordered-ns) (ut/remove-item min-val ns)))))
+
+(defn binary-sort-step
+  [traversed-items next-item-to-travese]
+  (if (empty? traversed-items)
+    [next-item-to-travese]
+    (if (> next-item-to-travese (first traversed-items))
+      (cons next-item-to-travese traversed-items)
+      (cons (first traversed-items) (cons next-item-to-travese (rest traversed-items))))))
+
+(defn traverse
+  [items]
+  (if (empty? items)
+    []
+    (reduce binary-sort-step [] items)))
+
 
 (defn rsort
-  "Sorts in reverse order the given numbers"
-  [numbers]
-  (process [] numbers))
+  "Sorts in reverse order using the bubble sort algorithm.
+  Example:
+  [1  4  2  3 ]
+  [1  4  3  2 ]
+  [1  4  3  2 ]
+  [4* 1  3  2 ]
+  [4* 1  3  2 ]
+  [4* 3* 1  2 ]
+  [4* 3* 2* 1 ]
+  [4* 3* 2* 1*]"
+  [items]
+  (if (empty? items)
+    []
+    (let [result (traverse items)]
+      (cons (first result) (rsort (rest result))))))
