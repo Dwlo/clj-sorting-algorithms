7373642740edf600f63a9e8e04435226584481d9
Dw <lo.dawid@gmail.com>
2015-01-31 22:51:26 +0100

    improve: improving readability on insertion sort

--- src/sorting_algorithms/insertion_sort.clj
+++ src/sorting_algorithms/insertion_sort.clj
@@ -3,21 +3,26 @@
 
 
 (defn insert
-  "Inserts a number into a ordered vector."
-  [ordered-ns n]
-  (if (empty? ordered-ns)
-    [n]
-    (if (> n (first ordered-ns))
-      (cons n ordered-ns)
-      (cons (first ordered-ns) (insert (rest ordered-ns) n)))))
+  "Inserts an item into a ordered vector."
+  [ordered-items item]
+  (if (empty? ordered-items)
+    [item]
+    (if (> item (first ordered-items))
+      (cons item ordered-items)
+      (cons (first ordered-items) (insert (rest ordered-items) item)))))
 
 (defn rsort
   "Sorts in reverse order using the insertion sort algorithm.
-  Example:
-  [1  4  2  3 ]
-  [4  1  2  3 ]
-  [4  2  1  3 ]
-  [4  2  3  1 ]
-  [4* 3* 2* 1*]"
-  [numbers]
-  (reduce (fn [ns n] (insert ns n)) [] numbers))
+  Steps:
+  |---------------+-----------------|
+  | ordered items | unordered items |
+  |---------------+-----------------|
+  |               |                 |
+  | [ ]           | [1  4  2  3 ]   |
+  | [1 ]          | [4  2  3 ]      |
+  | [4  1 ]       | [2  3 ]         |
+  | [4  2  1 ]    | [3 ]            |
+  | [4  3  2  1 ] | [ ]             |
+  |---------------+-----------------|"
+  [items]
+  (reduce (fn [ns n] (insert ns n)) [] items))
