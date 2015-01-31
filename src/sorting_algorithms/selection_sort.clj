f718593de09a6003b384e3504de01d7b63c6d144
Dw <lo.dawid@gmail.com>
2015-01-31 23:13:05 +0100

    improve: improving readability on selection sort

--- src/sorting_algorithms/selection_sort.clj
+++ src/sorting_algorithms/selection_sort.clj
@@ -1,21 +1,25 @@
 (ns sorting-algorithms.selection-sort
   "Sorting using the selection sort algorithm."
-  (:require [sorting-algorithms.utils :as  ut]))
+  (:require [sorting-algorithms.utils :refer [remove-item]]))
 
 (defn process
   [ordered-items messy-items]
   (if (empty? messy-items)
     ordered-items
     (let [min-val (apply min messy-items)]
-      (process (cons min-val ordered-items) (ut/remove-item min-val messy-items)))))
+      (process (cons min-val ordered-items) (remove-item min-val messy-items)))))
 
 (defn rsort
   "Sorts in reverse order using the selection sort algorithm.
-  Example:
-  [1  4  2  3 ]
-  [4* 1  2  3 ]
-  [4* 3* 1  2 ]
-  [4* 3* 2* 1 ]
-  [4* 3* 2* 1*]"
+  Steps:
+  |---------------+-----------------|
+  | ordered items | unordered items |
+  |---------------+-----------------|
+  | [ ]           | [1  4  2  3 ]   |
+  | [1 ]          | [4  2  3 ]      |
+  | [2  1 ]       | [4  3 ]         |
+  | [3  2  1 ]    | [4 ]            |
+  | [4  3  2  1 ] | [ ]             |
+  |---------------+-----------------|"
   [items]
   (process [] items))
