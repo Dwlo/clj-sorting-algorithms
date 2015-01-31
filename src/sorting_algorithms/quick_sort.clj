52f6e0efd68442900a6e087fc5af60d8952edd7c
Dw <lo.dawid@gmail.com>
2015-01-31 23:33:05 +0100

    improve: improving readability on insertion sort

--- src/sorting_algorithms/quick_sort.clj
+++ src/sorting_algorithms/quick_sort.clj
@@ -1,19 +1,21 @@
 (ns sorting-algorithms.quick-sort
-  (:require [sorting-algorithms.utils :as  ut]))
+  "Sorting using the quick sort algorithm."
+  (:require [sorting-algorithms.utils :refer [remove-item]]))
 
 
 (defn rsort
   "Sorts in reverse order using the quick sort algorithm.
-  Example:
+  Steps:
   [3  4  2  1  5 ]
   [4  5  3* 2  1 ]
   [5  4* 3* 2* 1 ]
   [5* 4* 3* 2* 1*]"
+
   [items]
   (if (empty? items)
     []
     (let [head            (first items)
-          remaining-items (ut/remove-item head items)
+          remaining-items (remove-item head items)
           greaters        (filter #(>  % head) remaining-items)
           smallers        (filter #(<= % head) remaining-items)]
       (concat (rsort greaters) [head] (rsort smallers)))))
