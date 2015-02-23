9d1cbff39acdd930f43e8e516a0a81729ca2df1a
Dw <lo.dawid@gmail.com>
2015-02-24 00:34:17 +0100

    intermediate commit: changing implementation to simulate sort and not reverse sort

--- src/sorting_algorithms/bubble_sort.clj
+++ src/sorting_algorithms/bubble_sort.clj
@@ -2,34 +2,50 @@
   "Sorting using the bubble sort algorithm.")
 
 
-(defn binary-sort-step
-  [traversed-items next-item-to-travese]
-  (if (empty? traversed-items)
-    [next-item-to-travese]
-    (if (> next-item-to-travese (first traversed-items))
-      (cons next-item-to-travese traversed-items)
-      (cons (first traversed-items) (cons next-item-to-travese (rest traversed-items))))))
+(defn bubblify
+  "Bubbles the given values."
+  [vals]
+  (-> (fn [sorted-vals next-val]
+        (cond
+         (empty? sorted-vals) [next-val]
+         (> next-val (peek sorted-vals)) (conj sorted-vals next-val)
+         :else (conj (pop sorted-vals) next-val (peek sorted-vals))))
+      (reduce [] vals)))
 
-(defn traverse
-  [items]
-  (if (empty? items)
-    []
-    (reduce binary-sort-step [] items)))
+(defn rearrange
+  "Rearranges the given values using the bubble sort method."
+  [sorted-vals vals]
+  (if (empty? vals)
+    sorted-vals
+    (let [bubbled-vals (bubblify vals)
+          last-val (peek bubbled-vals)
+          not-last-vals (pop bubbled-vals)]
+      (rearrange (cons last-val sorted-vals) not-last-vals))))
 
-
-(defn rsort
-  "Sorts in reverse order using the bubble sort algorithm.
+(defn sort
+  "Sorts the given values using the bubble sort algorithm.
   Example:
-  [1  4  2  3 ]
-  [1  4  3  2 ]
-  [1  4  3  2 ]
-  [4* 1  3  2 ]
-  [4* 1  3  2 ]
-  [4* 3* 1  2 ]
-  [4* 3* 2* 1 ]
-  [4* 3* 2* 1*]"
-  [items]
-  (if (empty? items)
-    []
-    (let [result (traverse items)]
-      (cons (first result) (rsort (rest result))))))
+  |----------------+----------------|
+  | sorted         | unsorted       |
+  |----------------+----------------|
+  | [ ]            | [ 2- 4- 1  3 ] |
+  | [ ]            | [ 2  4- 1- 3 ] |
+  | [ ]            | [ 2  1  4- 3-] |
+  | [ ]            | [ 2  1  3  4*] |
+  |----------------+----------------|
+  | [ 4 ]          | [ 2- 1- 3 ]    |
+  | [ 4 ]          | [ 1  2- 3-]    |
+  | [ 4 ]          | [ 1  2  3*]    |
+  |----------------+----------------|
+  | [ 3  4 ]       | [ 1- 2-]       |
+  | [ 3  4 ]       | [ 1  2*]       |
+  |----------------+----------------|
+  | [2  3  4 ]     | [ 1*]          |
+  |----------------+----------------|
+  | [ 1  2  3  4 ] | [ ]            |
+  |----------------+----------------|"
+  [vals]
+  (rearrange [] vals))
+
+(defn rsort [vals]
+  (reverse (sort vals)))
