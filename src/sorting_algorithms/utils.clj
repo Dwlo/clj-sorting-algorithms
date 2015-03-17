be39d8f1c2c1a67432a3827531143975b4dea608
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-03-17 09:21:17 +0100

    Refactor - Remove unneeded space and parenthesis

--- src/sorting_algorithms/utils.clj
+++ src/sorting_algorithms/utils.clj
@@ -5,6 +5,6 @@
 (defn remove-val
   "Removes the first given item from the vector."
   [n ns]
-  (->> (take-while #(not= % n ) ns)
+  (->> (take-while #(not= % n) ns)
        (merge (rest (drop-while #(not= % n) ns)))
-       (flatten)))
+       flatten))
