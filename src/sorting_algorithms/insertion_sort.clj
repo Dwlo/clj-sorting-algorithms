f149b1388b4d7a45e5726995b9d62e13cfa7d046
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-03-17 08:41:53 +0100

    Compute rest of unsorted values only when needed

--- src/sorting_algorithms/insertion_sort.clj
+++ src/sorting_algorithms/insertion_sort.clj
@@ -7,11 +7,11 @@
   [sorted-values value]
   (if (empty? sorted-values)
     [value]
-    (let [not-last-sorted-vals (pop sorted-values)
-          last-val (peek sorted-values)]
-        (if (>= value last-val)
+    (let [last-val (peek sorted-values)]
+      (if (>= value last-val)
         (conj sorted-values value)
-        (conj (insert-at-right-place not-last-sorted-vals value) last-val)))))
+        (let [not-last-sorted-vals (pop sorted-values)]
+          (conj (insert-at-right-place not-last-sorted-vals value) last-val))))))
 
 (defn sort
   "Sorts using the insertion sort algorithm.
