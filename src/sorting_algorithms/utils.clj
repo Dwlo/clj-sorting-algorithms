ed0a53ecf815842fe38906b0a373b2d27033840a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-03-17 18:55:17 +0100

    Refactor - Simplify using `split-with` standard fn

--- src/sorting_algorithms/utils.clj
+++ src/sorting_algorithms/utils.clj
@@ -5,6 +5,5 @@
 (defn remove-val
   "Removes the first given item from the vector."
   [n ns]
-  (let [not=n? #(not= % n)]
-    (-> (take-while not=n? ns)
-        (concat (rest (drop-while not=n? ns))))))
+  (let [[heads tails] (split-with #(not= % n) ns)]
+    (concat heads (rest tails))))
